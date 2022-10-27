b1c7a5e0d9e60bbd4817b00e478467ac04db2832
Kirill Menzurenko <menzurenko1998@gmail.com>
2022-10-27 19:33:25 +0300

    5 trial

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,39 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
+    public static String userName;
+    public static void congratulation() {
+        System.out.println("Congratulations, " + userName + "!");
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public static void greetings() {
+        System.out.print("Welcome to the Brain Games!\nMay I have your name? ");
+        Scanner scannerGreetings = new Scanner(System.in);
+        String userName1 = scannerGreetings.next();
+        App person = new App();
+        person.setUserName(userName1);
+        System.out.println("Hello, " + userName + "!");
+    }
+
     public static void main(String[] args) {
-        Cli.example();
+        System.out.println("Please enter the game number and press Enter.");
+        Scanner mainScanner = new Scanner(System.in);
+        System.out.println("1 - Greet\n2 - Even\n0 - Exit");
+        String userGameChoise = mainScanner.next();
+        System.out.println("Your choice: " + userGameChoise);
+        switch (userGameChoise) {
+            case("1"):
+                greetings();
+                break;
+            case("2"):
+                Even.numberGame();
+                break;
+            default:
+                return;
+        }
     }
 }
---
+++ app/src/main/java/hexlet/code/Even.java
@@ -0,0 +1,43 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Even {
+    public static void numberGame() {
+        String yesAnswer = "yes";
+        String noAnswer = "no";
+        Scanner scannerNumberGame = new Scanner(System.in);
+        App.greetings();
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        for (int i = 1; i <= 3; i++) {
+            int x = (int) (Math.random() * 1001);
+            System.out.println("Question: " + x);
+            String answer = scannerNumberGame.next();
+            System.out.println("Your answer: " + answer);
+            if (x % 2 == 0 & answer.equals(yesAnswer)) {
+                System.out.println("Correct!");
+            } else if (x % 2 != 0 & answer.equals(yesAnswer)) {
+                System.out.println("'yes' is wrong answer ;(. Correct answer was 'no'\n"
+                        + " Let's try again, " + App.userName);
+                return;
+            } else if (x % 2 == 0 & answer.equals(noAnswer)) {
+                System.out.println("'no' is wrong answer ;(. Correct answer was 'yes'\n"
+                        + " Let's try again, " + App.userName);
+                return;
+            } else if (x % 2 != 0 & answer.equals(noAnswer)) {
+                System.out.println("Correct!");
+            } else {
+                if (x % 2 != 0) {
+                    System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'no'\n"
+                            + " Let's try again, " + App.userName);
+                    return;
+                } else {
+                    System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was 'yes'\n"
+                            + " Let's try again, " + App.userName);
+                    return;
+                }
+            }
+        }
+        App.congratulation();
+    }
+}
